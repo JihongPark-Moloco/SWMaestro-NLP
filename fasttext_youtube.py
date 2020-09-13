9369ee0ea22d379013e797650db6bb55757303c7
박지홍 <qkrwlghddlek@naver.com>
2020-09-13 18:29:50 +0900

    Update fasttext_youtube.py, fasttext_youtube_comment.py
    각 영상의 키워드를 통해 패스트 텍스트를 학습하고 이를 저장하고 불러올 수 있도록 수정하였습니다.

--- fasttext_youtube.py
+++ fasttext_youtube.py
@@ -1,12 +1,7 @@
-from gensim import models
-import psycopg2 as pg2
-import re
-import pandas as pd
-import csv
 import numpy as np
-from scipy import spatial
-from sklearn import neighbors
+import pickle
 from scipy.spatial import distance
+from gensim import models
 
 # ## 추가 학습 시키는 소스
 # # IP = "ec2-13-124-107-195.ap-northeast-2.compute.amazonaws.com"
@@ -45,64 +40,71 @@ from scipy.spatial import distance
 #     print(f'{w}: {sim}')
 
 
-IP = "ec2-13-124-107-195.ap-northeast-2.compute.amazonaws.com"
-ko_model = models.fasttext.FastText.load('new_model')
+############# model 구현 소스 ###########
+#
+# IP = "ec2-13-124-107-195.ap-northeast-2.compute.amazonaws.com"
+# ko_model = models.fasttext.FastText.load('0908_model')
+#
+# conn = pg2.connect(database="createtrend", user="muna", password="muna112358!", host=IP,
+#                    port="5432")
+# conn.autocommit = False
+# cur = conn.cursor()
+# cur.execute(f"""SELECT idx FROM channel WHERE status = true""")
+# channel_list = cur.fetchall()
+#
+# channel_vector = []
+# index_to_name = dict()
+# index_to_idx = dict()
+# idx_to_index = dict()
+#
+# index = 0
+#
+# for channel_idxx in channel_list:
+#     channel_idx = channel_idxx[0]
+#
+#     cur.execute(f"""SELECT channel_name FROM channel WHERE idx = {channel_idx};""")
+#     channel_name = cur.fetchall()[0][0]
+#
+#     cur.execute(f"""SELECT * FROM (
+#            SELECT keyword, count(keyword) as cc
+#            FROM video_keyword
+#            WHERE video_idx IN (SELECT idx FROM video WHERE channel_idx = {channel_idx})
+#            GROUP BY keyword
+#            ORDER BY cc DESC
+#     ) as res WHERE res.cc >=2 LIMIT 100;""")
+#
+#     res = cur.fetchall()
+#     all = np.zeros(300)
+#     total_weight = 0
+#
+#     for keyword, weight in res:
+#         all += ko_model.wv.get_vector(keyword) * weight
+#         total_weight += weight
+#
+#     if total_weight == 0:
+#         continue
+#
+#     all /= total_weight
+#
+#     index_to_name[index] = channel_name
+#     index_to_idx[index] = channel_idx
+#     idx_to_index[channel_idx] = index
+#     channel_vector.append(all)
+#
+#     index += 1
+#
+# cnp = np.array(channel_vector)
+# with open('0908_data.pickle', 'wb') as f:
+#     pickle.dump([cnp, index_to_name, index_to_idx, idx_to_index], f)
 
-conn = pg2.connect(database="createtrend", user="muna", password="muna112358!", host=IP,
-                   port="5432")
-conn.autocommit = False
-cur = conn.cursor()
-cur.execute(f"""SELECT idx FROM channel;""")
-channel_list = cur.fetchall()
+with open('0908_data.pickle', 'rb') as f:
+    cnp, index_to_name, index_to_idx, idx_to_index = pickle.load(f)
 
-channel_vector = []
-index_to_name = []
-index_to_idx = []
-idx_to_index = dict()
-
-
-index = 0
-
-for channel_idxx in channel_list:
-    channel_idx = channel_idxx[0]
-
-    cur.execute(f"""SELECT channel_name FROM channel WHERE idx = {channel_idx};""")
-    channel_name = cur.fetchall()[0][0]
-
-    cur.execute(f"""SELECT keyword, count(keyword) as cc
-    FROM video_keyword
-    WHERE video_idx IN (SELECT idx FROM video WHERE channel_idx = {channel_idx})
-    GROUP BY keyword ORDER BY cc DESC LIMIT 10;""")
-
-    res = cur.fetchall()
-    all = np.zeros(300)
-    total_weight = 0
-
-    for keyword, weight in res:
-        all += ko_model.wv.get_vector(keyword) * weight
-        total_weight += weight
-
-    if total_weight == 0:
-        continue
-
-    all /= total_weight
-
-    index_to_name.append(channel_name)
-    index_to_idx.append(index)
-    idx_to_index[channel_idx] = index
-    channel_vector.append(all)
-
-    index += 1
-
-cnp = np.array(channel_vector)
-np.save('channel_vector_10', cnp)
-cnp = np.load('channel_vector_10.npy')
-
-
-distances = distance.cdist([cnp[idx_to_index[499]]], cnp, "cosine")[0]
-ind = np.argpartition(distances, 10)[:10]
-for i in np.argpartition(distances, range(10))[:10]:
-    print(index_to_name[i], distances[i])
+num_channels = 50
+distances = distance.cdist([cnp[idx_to_index[12]]], cnp, "cosine")[0]
+ind = np.argpartition(distances, num_channels)[:num_channels]
+for i in np.argpartition(distances, range(num_channels))[:num_channels]:
+    print(index_to_idx[i], index_to_name[i], distances[i])
 
 min_index = np.argmin(distances)
 min_distance = distances[min_index]
